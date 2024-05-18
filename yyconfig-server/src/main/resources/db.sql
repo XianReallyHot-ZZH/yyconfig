a14a32d9a5c3f9cb3a52a04dc0847f52b4d3052a
XianReallyHot-ZZH <741788769@qq.com>
2024-05-18 22:36:21 +0800

    feat:数据库由H2变更到MySQL

--- yyconfig-server/src/main/resources/db.sql
+++ yyconfig-server/src/main/resources/db.sql
@@ -1,4 +1,5 @@
 
+drop table if exists `configs`;
 create table if not exists `configs` (
     `app` varchar(64) not null,
     `env` varchar(64) not null,
@@ -11,3 +12,10 @@ insert into configs (app, env, ns, pkey, pval) values ('app1', 'dev', 'public', 
 insert into configs (app, env, ns, pkey, pval) values ('app1', 'dev', 'public', 'yy.b', 'http://localhost:9129');
 insert into configs (app, env, ns, pkey, pval) values ('app1', 'dev', 'public', 'yy.c', 'cc100');
 
+drop table if exists `locks`;
+create table if not exists `locks` (
+    `id` int primary key not null,
+    `app` varchar(64) not null
+);
+
+insert into locks (id, app) values (1, 'yyconfig-server');
